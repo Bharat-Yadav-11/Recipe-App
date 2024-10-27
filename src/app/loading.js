d49fcbb0c4f7062822a8b6fe9351b718a1f1e491
Bharat Bhushan <2201010148@krmu.edu.in>
2024-10-27 09:36:03 +0530

    update

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
 
     return (
-        <div className="w-full min-h-screen items-center justify-center">
+        <div className="grid m-0 grid-rows-[20px_1fr_20px] items-center h-[100vh] w-[100%] justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <h1 className="text-3xl">Loading Please Wait...</h1>
             <Skeleton />
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default async function Home() {
     <div className="grid m-0 grid-rows-[20px_1fr_20px] items-center overflow-x-hidden h-[100vh] w-[100%] justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#2563EA]">
       <div className="w-[92%] h-[90vh] mt-14 sm:w-[90%] md:w-[80%] lg:w-[70%] bg-white md:h-[70vh] absolute flex flex-col items-center md:items-start justify-center p-5 md:p-10 rounded-2xl text-center md:text-left">
 
-        <h1 className="relative md:bottom-24 bottom-8 text-xl font-bold">Food<span className="text-blue-500">Lovers</span></h1>
+        <h1 className="relative md:bottom-24 bottom-12 text-xl font-bold">Food<span className="text-blue-500">Lovers</span></h1>
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold">
           Learn. Cook. Share.<br />Cooking Made Easy
         </h1>
@@ -32,11 +32,11 @@ export default async function Home() {
         <Link href={'/recipe-list'} className="underline text-blue-700 mt-4">
           Go to Recipe List
         </Link>
-        <div className="md:absolute items-center justify-items-center mt-5 md:mt-0 md:top-1/2 md:right-10 md:transform md:-translate-y-1/2 w-full md:w-[40%]">
+        <div className="md:absolute items-center justify-items-center mt-10 md:mt-0 md:top-1/2 md:right-10 md:transform md:-translate-y-1/2 w-full md:w-[40%]">
           <img
             src={recipeImg?.image} 
             alt={recipeImg?.name || "Recipe image"}
-            className="h-52 md:h-full w-full md:w-full object-cover object-top rounded-xl shadow-lg"
+            className="h-51 md:h-full w-full md:w-full object-cover object-top rounded-xl shadow-lg"
           />
         </div>
 
